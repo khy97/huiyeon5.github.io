95607e141da1169293dcb98c9aa12476f90212af
Huiyeon Kim <huiyeon.kim.1228@gmail.com>
2022-04-12 23:50:09 +0800

    fix: update particle package to new tsparticles

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react';
-import { StaticQuery, graphql } from 'gatsby'
+import React, { useContext } from "react";
+import { StaticQuery, graphql } from "gatsby";
 
-import ParticlesWrapper from './ParticlesWrapper';
-import { ResumeContext } from '../pages/index';
+import ParticlesWrapper from "./ParticlesWrapper";
+import { ResumeContext } from "../pages/index";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import { MdLocationOn } from 'react-icons/md';
-import { GrLinkedin, GrGithub, GrMedium } from 'react-icons/gr'
+import { MdLocationOn } from "react-icons/md";
+import { GrLinkedin, GrGithub, GrMedium } from "react-icons/gr";
 import { IconContext } from "react-icons";
 
 import * as S from "../styles/panel.styles";
@@ -15,17 +15,18 @@ const Panel = () => {
   return (
     <StaticQuery
       query={imageQuery}
-      render={data => {
+      render={(data) => {
         return (
           <S.Panel>
+            <ParticlesWrapper />
             <S.IntroductionWrapper>
-              {!loading ?
+              {!loading ? (
                 <S.PanelImage fluid={data.file.childImageSharp.fluid} />
-                :
+              ) : (
                 <SkeletonTheme color="#181d44" highlightColor="#2c3184">
                   <Skeleton circle={true} height={150} width={150} />
                 </SkeletonTheme>
-              }
+              )}
               <S.Introduction>
                 <S.H1>Huiyeon Kim</S.H1>
                 <S.H2>Site Reliability Engineer @ Goldman Sachs</S.H2>
@@ -35,28 +36,62 @@ const Panel = () => {
                   </IconContext.Provider>
                   <span style={{ marginLeft: 5 }}>Singapore</span>
                 </div>
-                <S.Tagline>Welcome! I'm a Software Engineer, Technical Writer and a Tutor. </S.Tagline>
+                <S.Tagline>
+                  Welcome! I'm a Software Engineer, Technical Writer and a
+                  Tutor.{" "}
+                </S.Tagline>
                 <S.ContactInformation>
-                  <IconContext.Provider value={{ color: "white", size: 24, style: { cursor: `pointer` } }}>
-                    <GrLinkedin onClick={() => window.open('https://www.linkedin.com/in/huiyeonkim/', "_blank")} />
+                  <IconContext.Provider
+                    value={{
+                      color: "white",
+                      size: 24,
+                      style: { cursor: `pointer` },
+                    }}
+                  >
+                    <GrLinkedin
+                      onClick={() =>
+                        window.open(
+                          "https://www.linkedin.com/in/huiyeonkim/",
+                          "_blank"
+                        )
+                      }
+                    />
                   </IconContext.Provider>
-                  <IconContext.Provider value={{ color: "white", size: 24, style: { cursor: `pointer` } }}>
-                    <GrGithub onClick={() => window.open('github.com/huiyeon5/', "_blank")} />
+                  <IconContext.Provider
+                    value={{
+                      color: "white",
+                      size: 24,
+                      style: { cursor: `pointer` },
+                    }}
+                  >
+                    <GrGithub
+                      onClick={() =>
+                        window.open("https://github.com/huiyeon5/", "_blank")
+                      }
+                    />
                   </IconContext.Provider>
-                  <IconContext.Provider value={{ color: "white", size: 24, style: { cursor: `pointer` } }}>
-                    <GrMedium onClick={() => window.open('https://medium.com/techfront', "_blank")} />
+                  <IconContext.Provider
+                    value={{
+                      color: "white",
+                      size: 24,
+                      style: { cursor: `pointer` },
+                    }}
+                  >
+                    <GrMedium
+                      onClick={() =>
+                        window.open("https://medium.com/techfront", "_blank")
+                      }
+                    />
                   </IconContext.Provider>
                 </S.ContactInformation>
               </S.Introduction>
             </S.IntroductionWrapper>
-            <ParticlesWrapper />
           </S.Panel>
-        )
-      }
-      }
+        );
+      }}
     />
-  )
-}
+  );
+};
 
 export default Panel;
 
@@ -64,10 +99,10 @@ const imageQuery = graphql`
   query {
     file(relativePath: { eq: "Huiyeon Kim.jpg" }) {
       childImageSharp {
-        fluid(maxWidth:1800) {
+        fluid(maxWidth: 1800) {
           ...GatsbyImageSharpFluid
         }
       }
     }
   }
-`
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,63 @@
-import React, { useState, useEffect } from 'react'
-import Layout from '../components/layout'
-import Panel from '../components/Panel'
-import SEO from '../components/seo'
-import Content from '../components/content'
-import '../styles/index.css'
+import React, { useState, useEffect } from "react";
+import Layout from "../components/layout";
+import Panel from "../components/Panel";
+import SEO from "../components/seo";
+import Content from "../components/content";
+import "../styles/index.css";
 
 const ResumeContext = React.createContext();
 
 const IndexPage = () => {
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
 
   useEffect(() => {
     const url = `https://gitconnected.com/v1/portfolio/huiyeon5`;
     const fetchData = async () => {
       try {
-        const resumeData = await fetch(url)
-        const output = await resumeData.json()
-        setData(output)
-        setLoading(false)
+        const resumeData = await fetch(url);
+        const output = await resumeData.json();
+        setData(output);
+        setLoading(false);
       } catch (e) {
         if (e) {
-          console.log(e.message, 'Try updating the API key in App.js')
+          console.log(e.message, "Try updating the API key in App.js");
         }
       }
-    }
+    };
     fetchData();
-  }, [])
-
+  }, []);
 
   return (
     <Layout>
-      <ResumeContext.Provider value={{
-        loading,
-        data
-      }}>
-        <SEO title="Huiyeon Kim" keywords={[`Huiyeon Kim`, `Developer`, `Writer`, `Personal Blog`, 'Software Developer', `Projects`, `Student`, `Tech Enthusiast`, `Kim`, `Huiyeon`]} image={require('../images/Huiyeon Kim.jpg')} />
+      <ResumeContext.Provider
+        value={{
+          loading,
+          data,
+        }}
+      >
+        <SEO
+          title="Huiyeon Kim"
+          keywords={[
+            `Huiyeon Kim`,
+            `Developer`,
+            `Writer`,
+            `Personal Blog`,
+            "Software Developer",
+            `Projects`,
+            `Student`,
+            `Tech Enthusiast`,
+            `Kim`,
+            `Huiyeon`,
+          ]}
+          image={require("../images/Huiyeon Kim.jpg")}
+        />
         <Panel />
         <Content />
       </ResumeContext.Provider>
     </Layout>
-  )
-}
+  );
+};
 
-export { ResumeContext }
-export default IndexPage
+export { ResumeContext };
+export default IndexPage;
